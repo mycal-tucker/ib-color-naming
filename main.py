7f5140a6e451ed30bd3ecb3e5bc15c0294f00532
Mycal Tucker <mycal@mit.edu>
2022-05-03 15:49:37 -0400

    Fix import typos.
    Fix an import typo and add slightly different plotting and printing
    for accuracy and complexity.

--- main.py
+++ main.py
@@ -1,37 +1,43 @@
-import numpy as np
+import pickle
+
 import matplotlib.pyplot as plt
+
 import ib_naming_model
 from tools import *
-from figrues import mode_map
 
 LOGGER = get_logger('main')
 
 
 def main():
-    # load model
-    model = ib_naming_model.load_model()
+    # Load model. Leave blank for a default model, or pass in a path to a custom model as done here.
+    model = ib_naming_model.load_model('models/IB_color_naming_model/unif_model.pkl')
     curve = model.IB_curve
 
-    # theoretical bound
+    with open('data/pw_m', 'rb') as file:
+        pw_m = pickle.load(file)
+    eps, gnid, bl, qw_m_fit = model.fit(pw_m)
+    acc = model.accuracy(pw_m)
+    comp = model.complexity(pw_m)
+    print("Acc", acc)
+    print("Comp", comp)
+
+    # Plot the theoretical bound and the actual point
     plt.figure()
     plt.plot(curve[0], curve[1])
     plt.xlabel('complexity, $I(M;W)$')
     plt.ylabel('accuracy, $I(W;U)$')
+    plt.scatter(comp, acc)  # The actual point that was recorded.
     plt.xlim([0, H(model.pM)])
     plt.ylim([0, model.I_MU + 0.1])
-
-    # generate fake (random) data and fit an optimal IB system
-    pW_M_fake = np.random.rand(330, 10)
-    pW_M_fake /= pW_M_fake.sum(axis=1)[:, None]
-    print(model.fit(pW_M_fake)[:-1])
+    plt.show()
 
     # let's plot a mode map!
-    indx = 500
-    qW_M = model.qW_M[indx]
+    qW_M = qw_m_fit
     plt.figure(figsize=(6.4, 2.5))
     model.mode_map(qW_M)
-    plt.title('Optimal IB system for $\\beta = %.3f$' % model.betas[indx])
+    plt.title('Optimal IB system for $\\beta = %.3f$' % bl)
     plt.tight_layout()
+    plt.show()
     print(model.complexity(qW_M), model.accuracy(qW_M))
 
 
